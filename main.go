64244ebb9fe2a40da1f5c727353a581034bb1fe4
Mark Samman <mark.samman@gmail.com>
2014-03-25 11:08:00 -0400

    Open torrent file

--- main.go
+++ main.go
@@ -1,7 +1,24 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"os"
+)
+
+func printHelp() {
+	fmt.Println("usage: gotorrent file")
+}
 
 func main() {
-	fmt.Println("Hello world")
+	if len(os.Args) <= 1 {
+		printHelp()
+		os.Exit(0)
+	}
+
+	file, err := os.Open(os.Args[1])
+	if err != nil {
+		log.Fatal(err)
+	}
+	file.Close()
 }
