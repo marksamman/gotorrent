8ea1b2e302371a238b76b5eb555cc7e2ff911912
Mark Samman <mark.samman@gmail.com>
2015-07-06 15:40:57 +0200

    Fix bug where a file would be re-created when failing to open

--- main.go
+++ main.go
@@ -47,24 +47,40 @@ type FileWriterMessage struct {
 	torrent  *Torrent
 }
 
+func resendFileWriterMessage(message *FileWriterMessage) {
+	fileWriterChannel <- message
+}
+
 func fileWriterListener() {
 	for {
 		message := <-fileWriterChannel
 
 		file, err := os.OpenFile(message.filename, os.O_WRONLY, 0600)
 		if err != nil {
-			if file, err = os.Create(message.filename); err != nil {
-				log.Printf("Failed to write data to %s: %s", message.filename, err)
+			if os.IsNotExist(err) {
+				if file, err = os.Create(message.filename); err != nil {
+					log.Printf("Failed to create file %s: %s", message.filename, err)
+					go resendFileWriterMessage(message)
+					continue
+				}
+			} else {
+				log.Printf("Failed to open file %s: %s", message.filename, err)
+				go resendFileWriterMessage(message)
 				continue
 			}
 		}
 
-		file.WriteAt(message.data, message.offset)
+		if _, err := file.WriteAt(message.data, message.offset); err != nil {
+			log.Printf("Failed to write to file %s: %s", message.filename, err)
+			go resendFileWriterMessage(message)
+			continue
+		}
+
 		file.Close()
 
-		go func() {
-			message.torrent.fileWriteDone <- struct{}{}
-		}()
+		go func(fileWriteDone chan struct{}) {
+			fileWriteDone <- struct{}{}
+		}(message.torrent.fileWriteDone)
 	}
 }
 
